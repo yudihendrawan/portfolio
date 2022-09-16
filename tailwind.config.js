35656c9d9b4ccb7da413f85d529cb69a29d37671
yudihendrawan <yudihendrawan04@gmail.com>
2022-09-17 00:54:40 +0700

    Add: setup project and add content index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./public/**/*.{html,js,php}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        primary : '#0ea5e9',
+      },
+      fontFamily:{
+        inter : ['Inter'],
+      },
+    },
   },
   plugins: [],
 }
